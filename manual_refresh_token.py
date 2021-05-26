4a9a957a2ec621dbb517264c2afdb24a4deb5bcd
usr7m <78221542+usr7m@users.noreply.github.com>
2021-05-26 07:52:02 -0400

    Update manual_refresh_token.py

--- manual_refresh_token.py
+++ manual_refresh_token.py
@@ -6,27 +6,22 @@ import requests
 import time
 
 ''' API client_ID auth stuff: '''
+client_id = 'CLIENT_ID' # fill this out
+path_to_token_file = '/path/to/token.file'
+redirect_uri = 'https://Localhost'
 
-# fill this out with your ID, JUST ID, nothing else. no @AMER.OA.... no, just ID
-client_id = 'CLIENT_ID'           
-redirect_uri = 'http://Localhost'
-
-''' chromium options and such '''
 
 options = webdriver.ChromeOptions()
 options.binary_location = which('chromium')
-chrome_driver_binary = '/PATH_TO/chromedriver'   # you will need a chromedriver for this (easily obtained), or work around manually
+chrome_driver_binary = '/path/to/chromedriver'
 driver = webdriver.Chrome(chrome_driver_binary, options=options)
 
-url = 'https://auth.tdameritrade.com/auth?response_type=code&redirect_uri='\ 
-        + redirect_uri + '&client_id=' + client_id + '%40AMER.OAUTHAP'
+url = 'https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=' + redirect_uri\
+	+ '&client_id=' + client_id + '%40AMER.OAUTHAP'
 
 driver.get(url)
 
-''' this part can be skipped
-    and all forms filled out on the web as directed until... '''
-    
-print('TDA username: ')     # 
+print('TDA username: ')
 tdauser = input()
 print('TDA password: ')
 tdapass = input()
@@ -45,17 +40,22 @@ driver.find_element_by_id('accept').click()
 driver.find_element_by_id('trustthisdevice0_0').click()
 driver.find_element_by_id('accept').click()
 
-''' until HERE .... this is where your code appears in the URL '''
-
-''' fetch and decode the code '''
-
 code = up.unquote(driver.current_url.split('code=')[1])
-print(code)
 driver.close()
 
-''' get the access and refresh tokens via obtained code '''
 
-resp = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
+print(code)
+
+
+
+def get_today_timestamp():
+   today = datetime.date.today()
+   today = pd.to_datetime(today).timestamp()
+   return int(today)
+
+
+def both_tokens_by_code(client_id, code):
+   resp = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
                      headers={'Content-Type': 'application/x-www-form-urlencoded'},
                      data={'grant_type': 'authorization_code',
                            'refresh_token': '',
@@ -63,20 +63,19 @@ resp = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
                            'code': code,
                            'client_id': client_id,
                            'redirect_uri': redirect_uri})
-print(resp.status_code)
-print(resp.json())
-access_token = resp.json()['access_token']
-refresh_token = resp.json()['refresh_token']
-token_type = resp.json()['token_type']
+   return (resp.json())
 
-''' a quick function to put the refresh token into a txt file '''
 
-def create_rtok_file(refresh_token):
-	f_path = '/PATH_TO_NEW_TOKEN_FILE/'           # fill this out with your own path to file
-	f = open(f_path + 'refresh_token.txt', 'w')   
-	f.write(refresh_token)
-	f.close()
-
-''' call it '''
-
-create_rtok_file(refresh_token)
+def update_tokens_by_code():
+   tokens = both_tokens_by_code(client_id, code)
+   if 'access_token' in tokens:
+      del tokens['access_token']
+      del tokens['expires_in']
+   today = get_today_timestamp()
+   refresh_token_expires_in = tokens['refresh_token_expires_in']
+   refresh_token_expires_ON = int(today + refresh_token_expires_in)  
+   print(refresh_token_expires_ON)
+   tokens['refresh_token_expires_ON'] = refresh_token_expires_ON
+   with open(path_to_token_file + 'token_file', 'w') as token_file:
+      json.dump(tokens, token_file)
+   print('token_file created.')
